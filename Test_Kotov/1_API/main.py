4804914cb6e80ac900599457ee1c97c7077d30ae
ayshe <ayshenko@gmail.com>
2021-12-19 19:55:53 +0300

    Test_API_dz

--- Test_Kotov/1_API/main.py
+++ Test_Kotov/1_API/main.py
@@ -2,3 +2,6 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
+
+responseHello = requests.get("https://playground.learnqa.ru/api/get_text")
+print(responseHello.text)
